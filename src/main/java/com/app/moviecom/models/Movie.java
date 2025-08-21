c1820bf9142552954da4c6b5e4eabdb542c3924f
Dinh773 <mitcamlove@gmail.com>
2025-08-21 13:08:06 +0200

    fix errors

--- src/main/java/com/app/moviecom/models/Movie.java
+++ src/main/java/com/app/moviecom/models/Movie.java
@@ -10,7 +10,7 @@ import lombok.Data;
 @Data
 public class Movie {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
     private double rating;
